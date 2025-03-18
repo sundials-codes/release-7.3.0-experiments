f2229085b35ec54768f21cd1109cb7b9babd47cc
Steven Roberts <roberts115@llnl.gov>
2025-03-18 15:10:00 -0700

    Update step range

--- Gray-Scott/splitting.sh
+++ Gray-Scott/splitting.sh
@@ -4,20 +4,20 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=50
-#SBATCH --array=0-39
+#SBATCH --array=0-29
 
 ORDERS=("1" "2" "3" "4" "6")
 
 DT_INDEX=$((SLURM_ARRAY_TASK_ID / ${#ORDERS[@]}))
 ORDER_INDEX=$((SLURM_ARRAY_TASK_ID % ${#ORDERS[@]}))
 
-DT=$(echo "2^(-2-$DT_INDEX)" | bc -l)
+DT=$(echo "2^(-4-$DT_INDEX)" | bc -l)
 ORDER=${ORDERS[$ORDER_INDEX]}
 
 mkdir -p data
 
 ./build/gray_scott \
-  grid_pts_1d 512 \
+  grid_pts_1d 1024 \
   method Splitting \
   order $ORDER \
   dt $DT \
