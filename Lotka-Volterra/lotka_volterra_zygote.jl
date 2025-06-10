72d0de04b2b31c5971af20742ea41854b4854481
Balos, Cody, J <balos1@llnl.gov>
2025-06-10 16:21:49 -0700

    update scripts with more tests/results

--- Lotka-Volterra/lotka_volterra_zygote.jl
+++ Lotka-Volterra/lotka_volterra_zygote.jl
@@ -21,15 +21,17 @@
 
 using Pkg
 
-# Pkg.add([
-#     PackageSpec(name="OrdinaryDiffEq"),
-#     PackageSpec(name="SciMLSensitivity"),
-#     PackageSpec(name="ForwardDiff"),
-#     PackageSpec(name="Zygote"),
-#     PackageSpec(name="Plots")
-# ])
+Pkg.add([
+    PackageSpec(name="Printf"),
+    PackageSpec(name="ArgParse"),
+    PackageSpec(name="OrdinaryDiffEq"),
+    PackageSpec(name="SciMLSensitivity"),
+    PackageSpec(name="ForwardDiff"),
+    PackageSpec(name="Zygote"),
+    PackageSpec(name="Plots")
+])
 
-using Printf, Sundials, LinearAlgebra, OrdinaryDiffEq, SciMLSensitivity, ForwardDiff, Zygote, Plots
+using ArgParse, Printf, LinearAlgebra, OrdinaryDiffEq, SciMLSensitivity, ForwardDiff, Zygote, Plots
 
 # Lotka Volterra with 4 parameters
 function f(du, u, p, t)
@@ -59,47 +61,75 @@ function dgdp!(out, u, p, t)
     out[4] = 0
 end
 
+# --------- Command line argument handling
+
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--order"
+        help = "Order of the ODE solver method (e.g., 3 for BS3, 4 for RK4, 5 for Tsit5)"
+        arg_type = Int
+        default = 3
+        "--dt"
+        help = "Timestep size (default=0.0 - which means adaptive)"
+        arg_type = Float64
+        default = 0.0
+    end
+    return parse_args(s)
+end
+
+args = parse_commandline()
+method_order = args["order"]
+dt = args["dt"]
+adaptive = false
+
+if method_order == 3
+    method = BS3()
+elseif method_order == 4
+    method = RK4()
+elseif method_order == 5
+    method = Tsit5()
+else
+    error("Unsupported method order: $method_order. Supported orders are 3 (BS3), 4 (RK4), 5 (Tsit5).")
+end
+
+if dt == 0.0
+    adaptive = true
+else
+    adaptive = false
+end
+
 # --------- Setup Forward Problem
 
 # Problem specification
 p = [1.5, 1.0, 3.0, 1.0]
 u0 = [1.0; 1.0]
 tspan = (0.0, 10.0)
-rtol = 1e-14
-atol = 1e-14
-# method = BS3()
-# method = RK4()
-# method = Tsit5()
-method = ARKODE(Sundials.Explicit(),etable = Sundials.BOGACKI_SHAMPINE_4_2_3)
-# dt = 1e-8
-dt = 0.25
+rtol = 1e-15
+atol = 1e-15
 
 # Integrate with OrdinaryDiffEq
 prob = ODEProblem(f, u0, tspan, p)
-# abstol=atol, reltol=rtol
-sol = solve(prob, method, dt=dt)
+sol = solve(prob, method, dt=dt, adaptive=adaptive)
+sol = solve(prob, method, dt=dt, adaptive=adaptive, abstol=atol, reltol=rtol)
 println("OrdinaryDiffEq computed solution: ", sol.u[end])
-@printf("%s: %.16e\n", "OrdinaryDiffEq computed ||u(t_f)||", norm(sol.u[end]))
+@printf("OrdinaryDiffEq computed ||u(%g)||: %.16e\n", sol.t[end], norm(sol.u[end]))
 
-# # term_cond = [0.0, 0.0, 0.0, 0.0] 
-# # dgdu_discrete!(term_cond, sol.u[end], p, 10., 0)
-# # @printf("%s: %.16e", "Adjoint terminal condition", term_cond)
+# Plot forward solution
+plot(sol)
+savefig("lotka_volterra_plot.png")
 
-# # Plot forward solution
-# plot(sol)
-# savefig("lotka_volterra_plot.png")
+# --------- Setup Adjoint Problem
 
-# # --------- Setup Adjoint Problem
+ts = tspan
 
-# ts = tspan
-
-# # Solve adjoint problem with reverse-mode automatic differentiation
-# function G(u, p)
-#     tmp_prob = remake(prob, u0=u, p=p)
-#     sol = solve(tmp_prob, method, dt=dt, saveat=ts)
-#     A = convert(Array, sol)
-#     g(A, p)
-# end
-# res3 = Zygote.gradient(G, u0, p)
-# @printf("%s: %.16e, %.16e\n", "Discrete Zygote (reverse mode) computed sensitivities L2 norm", norm(res3[1]), norm(res3[2]))
+# Solve adjoint problem with reverse-mode automatic differentiation
+function G(u, p)
+    tmp_prob = remake(prob, u0=u, p=p)
+    sol = solve(tmp_prob, method, dt=dt, adaptive=adaptive, saveat=ts)
+    A = convert(Array, sol)
+    g(A, p)
+end
+res3 = Zygote.gradient(G, u0, p)
+@printf("%s: %.16e, %.16e\n", "Discrete Zygote (reverse mode) computed sensitivities L2 norm", norm(res3[1]), norm(res3[2]))
 
